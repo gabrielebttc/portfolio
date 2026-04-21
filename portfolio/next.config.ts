286d7bc9644708ac6a49c3eb4e8057038bc3ff9e
Ubuntu <ubuntu@vps-55e47c18.vps.ovh.net>
2026-04-21 21:36:19 +0000

    removed watchOptions

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  watchOptions: {
-    usePolling: true,
-    interval: 500,
-  },
 };
 
 export default nextConfig;
